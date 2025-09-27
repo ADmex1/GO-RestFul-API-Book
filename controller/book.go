867e3d073c2cdc9fcb696a523692095df109d915
Viktor <157784316+ADmex1@users.noreply.github.com>
2025-09-28 00:48:45 +0800

    minor fix for per id search

--- controller/book.go
+++ controller/book.go
@@ -31,7 +31,6 @@ func AddBook(c *fiber.Ctx) error {
 func BookPerId(c *fiber.Ctx) error {
 	id := c.Params("id")
 	var books model.Book
-	database.DB.Find(&books)
 	if result := database.DB.First(&books, id); result.Error != nil {
 		return c.Status(404).JSON(fiber.Map{
 			"Error": "Book not found",
