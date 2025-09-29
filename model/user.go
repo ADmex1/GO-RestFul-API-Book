5c6274bfb7983ef6f014a58471a1ddf7f14a85f6
Viktor <157784316+ADmex1@users.noreply.github.com>
2025-09-29 15:06:26 +0800

    Fixed uuid gorm

--- model/user.go
+++ model/user.go
@@ -9,7 +9,7 @@ import (
 
 type User struct {
 	InternalID int64          `json:"internal_id" gorm:"primaryKey"`
-	PublicID   uuid.UUID      `json:"public_id" gorm:"type:uuid"`
+	PublicID   uuid.UUID      `json:"public_id" gorm:"type:uuid;default:uuid_generate_v4()"`
 	Name       string         `json:"name" db:"name"`
 	Email      string         `json:"email" db:"email" gorm:"unique"`
 	Password   string         `json:"-"  gorm:"column:password"`
